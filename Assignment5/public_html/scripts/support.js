f494f1b60a174a86f7bf346aac280e6fcd395ebd
Alex Boyce <alexanderfboyceweb3@gmail.com>
2024-02-05 14:50:26 -0800

    got the first support part done; still need to add a date to the footer

--- Assignment5/public_html/scripts/support.js
+++ Assignment5/public_html/scripts/support.js
@@ -1,54 +1,30 @@
-//support.js
+//support.js script
+
 "use strict";
 
 // Define the $() function
-function $(selector) {
-    return document.querySelector(selector);
-}
+const $ = (selector) => document.querySelector(selector);
 
+document.addEventListener("DOMContentLoaded", () =>
+{
+    document.querySelectorAll("#faq h2").forEach((h2) =>
+    {
+        h2.addEventListener("click", () =>
+        {
 
-// Event handler for DOMContentLoaded
-document.addEventListener('DOMContentLoaded', function () {
-    // Attach the calculate function to the submit event of the form
-    $('section').addEventListener('click', function (event) {
-        event.preventDefault(); // Prevent the default form submission
-        calculate(); // Call the calculate function
+            h2.classList.toggle("minus");
+
+            const div = h2.nextElementSibling;
+
+            if (!h2.classList.contains("minus"))
+            {
+                div.style.display = "none";
+                h2.style.color = "#9b59b6";
+            } else
+            {
+                div.style.display = "block";
+                h2.style.color = "blue";
+            }
+        });
     });
-
-    // Move focus to the product cost text box
-    $('#uInput').focus();
 });
-
-$(document).ready(function() {
-    $("#categories h2").click( evt => {
-        const h2 = evt.currentTarget;
-
-        $(h2).toggleClass("minus");
-
-        if ($(h2).attr("class") !== "minus") {
-            $(h2).next().hide();
-        }
-        else {
-            $(h2).next().show();
-        }
-
-        $("#image").attr("src", "");
-    }); // end click
-
-    $("#web_images a, #language_images a, #database_images a").each((index, img) => {
-        // get old and new urls
-        var imageURL = $(img).attr("href");
-
-        // preload images		
-        var bookImage = new Image();
-        bookImage.src = imageURL;
-
-        // set up event handlers			
-        $(img).click( evt => {
-            $("#image").attr("src", imageURL);
-
-        // cancel the default action of each link
-        evt.preventDefault();
-        }); // end click 
-    }); // end each
-}); // end ready
