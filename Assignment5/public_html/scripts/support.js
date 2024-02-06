b0a78a262ff544ad79175b3c04d07beabd2778c5
nood-leog <alexanderfboyceweb3@gmail.com>
2024-02-06 11:11:10 -0800

    final commit, should be done correctly now

--- Assignment5/public_html/scripts/support.js
+++ Assignment5/public_html/scripts/support.js
@@ -2,29 +2,40 @@
 
 "use strict";
 
-// Define the $() function
-const $ = (selector) => document.querySelector(selector);
-
-document.addEventListener("DOMContentLoaded", () =>
+document.addEventListener("DOMContentLoaded", () => 
 {
-    document.querySelectorAll("#faq h2").forEach((h2) =>
+    const faqSection = document.querySelectorAll("#faq h2");
+
+    faqSection.forEach((h2) => 
     {
-        h2.addEventListener("click", () =>
+        h2.addEventListener("click", () => 
         {
+            //close section in advance of opening a new one
+            faqSection.forEach((otherH2) => 
+            {
+                if (otherH2 !== h2) 
+                {
+                    otherH2.classList.remove("minus");
+                    otherH2.nextElementSibling.style.display = "none";
+                    otherH2.style.color = "#9b59b6";
+                }
+            });
 
+            //toggle clicked section to open
             h2.classList.toggle("minus");
-
             const div = h2.nextElementSibling;
 
-            if (!h2.classList.contains("minus"))
-            {
-                div.style.display = "none";
-                h2.style.color = "#9b59b6";
-            } else
+            if (h2.classList.contains("minus")) 
             {
                 div.style.display = "block";
                 h2.style.color = "blue";
+            } 
+            else 
+            {
+                div.style.display = "none";
+                h2.style.color = "#9b59b6";
             }
         });
     });
 });
+
