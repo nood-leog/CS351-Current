3bbc23c5b2a23381c8fe5eaf8f8672d8afccdfc5
Alex Boyce <alexanderfboyceweb3@gmail.com>
2024-02-14 08:15:25 -0800

    support page functional, still working on bxslider

---
+++ Assignment6/public_html/scripts/guitars.js
@@ -0,0 +1,31 @@
+/* guitars js*/
+
+"use strict";
+
+$(document).ready(function(){
+  $('.bxslider').bxSlider();
+});
+
+/*
+$(document).ready(function ()
+{
+    $('.bxslider').bxSlider(
+            {
+                pager: true, // Display navigation dots
+
+                slideWidth: 400, // Adjust slide width if needed
+
+                auto: true, // Enable auto-scrolling
+
+                autoControls: true, // Show start/stop controls
+
+                autoStart: true, // Start auto-scrolling immediately
+
+                pause: 3000, // Auto-scroll interval in milliseconds
+
+                mode: 'horizontal' // Default horizontal scrolling
+                        
+            });
+});
+ * 
+ */
