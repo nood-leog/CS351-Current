6053d1d80f4c2987fcf72b86554dd0bb98e369b3
nood-leog <alexanderfboyceweb3@gmail.com>
2024-02-15 10:32:30 -0800

    Another Asg7 Test commit

--- Assignment7/index.php
+++ Assignment7/index.php
@@ -4,7 +4,7 @@
 
 <html lang="en">
     <head>
-        <title> Assignment 7 Alex Boyce </title>
+        <title> Assignment 7 Alex Boyce, and a test to see if git works</title>
         <link rel="stylesheet" href=./styles/main.css>
         <script src="./scripts/date.js"></script>
     </head>
