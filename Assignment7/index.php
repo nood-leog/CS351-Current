c101bf4443031ffe1a95a58bee1381fcc7b8fe03
Alex <116304896+nood-leog@users.noreply.github.com>
2024-02-15 10:41:08 -0800

    last pull test asg7

--- Assignment7/index.php
+++ Assignment7/index.php
@@ -4,7 +4,7 @@
 
 <html lang="en">
     <head>
-        <title> Assignment 7 Alex Boyce, and a last push test/title>
+        <title> Assignment 7 Alex Boyce, and a last pull test/title>
         <link rel="stylesheet" href=./styles/main.css>
         <script src="./scripts/date.js"></script>
     </head>
