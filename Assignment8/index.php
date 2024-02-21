29c19321557ab9ecf8a04c66d843d67cd841c778
nood-leog <alexanderfboyceweb3@gmail.com>
2024-02-21 10:33:20 -0800

    Test commit

---
+++ Assignment8/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<!-- Main Site Root index -->
+<!-- NO STYLE in HTML-->
+
+<html lang="en">
+    <head>
+        <title> Assignment 8 Alex Boyce</title>
+        <link rel="stylesheet" href=./styles/main.css>
+        <script src="./scripts/date.js"></script>
+    </head>
+    <body>
+        <header>
+            <img src="./images/FenderStratocaster.png" alt="Fender Stratocaster">
+            <h2>The Guitar Store</h2>
+            <h3>For rock stars everywhere!</h3>    
+        </header>
+
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="index.php">Lessons</a></li>
+                <li><a href="products/index.php">Products</a></li>
+                <li><a href="support/index.php">Support</a></li>
+                <li><a href="shipping/index.php">Shipping</a></li>
+                <li><a href="index.php">Contact Us</a>
+                    <ul>
+                        <li><a href="index.php">Email</a></li>
+                        <li><a href="index.php">Phone</a></li>
+                        <li><a href="index.php">Live Chat</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+
+        <main>
+            <section>
+                <h1>Welcome to the Guitar Store!</h1>
+                <p>Why buy from the Guitar Store? We have the world's largest selection of guitars, plus lots of other musical instruments. We let you try before you buy, so you can ensure the instrument is right for you. Our website is easy to navigate and, if you need help, an associate is seconds away. Finally, we can also help you learn to play an instrument. If you already know how, we can help you improve!</p>
+                <p>For expert advice, you can call us at 855-770-3373 or click the Live Chat button and an associate will be right with you.</p>
+
+                <h2>Featured Product</h2>
+                <a href="index.php"><img src="./images/LudwigDrumSet.png" alt="Ludwig Drum Set"></a>
+
+                <h3><a href="index.php">Ludwig Element Evolution 5-piece Drum Set - Red Sparkle Finish</a></h3>
+
+                <h3>Our guarantee</h3>
+                <p>If you aren't completely satisfied with anything you buy from our store, you can return it for a full refund.<b> No questions asked! </b></p>
+
+            </section>
+            <aside>
+                <ul>
+                    <li><a href="products/guitars/index.php">Guitars</a></li>
+                    <li><a href="index.php">Basses</a></li>
+                    <li><a href="index.php">Drums</a></li>
+                    <li><a href="index.php">Keyboards</a></li>
+                </ul>
+            </aside> 
+        </main>
+
+        <footer>
+            <p>
+                &copy; 2023 The Guitar Store 
+                <span id="currentDate"></span>
+            </p>
+        </footer>
+    </body>
+</html>
