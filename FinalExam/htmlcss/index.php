e2f2036e5f85b50893d47dae5d0003a48438ac4a
nood-leog <alexanderfboyceweb3@gmail.com>
2024-03-12 08:44:42 -0700

    htmlcss part done

--- FinalExam/htmlcss/index.php
+++ FinalExam/htmlcss/index.php
@@ -12,13 +12,23 @@
         </header>
 
         <main>
-            <?php include ('./view/aside.php'); ?>
             <!-- Your code should only be within main -->
-            Exam Home
-            Product of the week
-            cat1.jpg
-            20" Scary cat
-            Trick or Treat!
+
+            <aside>
+                <nav>
+                    <ul>
+                        <li><a href="./index.php">Exam Home</a></li>
+                    </ul>
+                </nav>
+            </aside> 
+            <section>
+                <h2>Product of the week</h2>
+                <a href="/FinalExam/htmlcss/under_construction.php"><img src="/FinalExam/htmlcss/images/cat1.jpg" alt="Scary cat"></a>
+                <br>
+                <a href="/FinalExam/htmlcss/under_construction.php">20" Scary cat</a>
+                <h3>Trick or Treat!</h3>
+                
+            </section>
         </main>
         <footer>
             <p>HTML/CSS</p>
