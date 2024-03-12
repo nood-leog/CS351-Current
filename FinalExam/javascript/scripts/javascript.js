fbf023858fd1ea08d4887247fa0cc986c22a66a3
nood-leog <alexanderfboyceweb3@gmail.com>
2024-03-12 09:29:18 -0700

    js part done

--- FinalExam/javascript/scripts/javascript.js
+++ FinalExam/javascript/scripts/javascript.js
@@ -5,7 +5,34 @@ const $ = selector => document.querySelector(selector);
 
 const validateInfo = (evt) => {
     /* Your code should be in this function */
+    
+    //values from the text boxes
+    let firstName = $('#first_name').value.trim();
+    let lastName = $('#last_name').value.trim();
+    let height = parseInt($('#height').value);
 
+    //first name
+    if (firstName === ''){
+    alert('Must enter first name.');
+    evt.preventDefault();
+    return;
+    }
+    
+    //last name
+    if (lastName === ''){
+     alert('Must enter last name.');
+     evt.preventDefault();
+     return;
+    }
+    
+    //validate height
+    if (height < 24 || height > 110 || isNaN(height)) { 
+        alert('Must enter height between 24 and 110.');
+        evt.preventDefault();
+        return; 
+    }
+
+    alert('Information entered correctly, returning to the home page.');
 };
 
 
